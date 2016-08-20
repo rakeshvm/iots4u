2a397e45770303c5526fc5291bc882b8318aa42f
shashidhar <shashidhar.e@gmail.com>
2016-08-20 15:27:41 +0530

    mail integrated

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/hello@iots4u.com",
                 type: "POST",
                 data: {
                     name: name,
